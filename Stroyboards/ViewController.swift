23eab09b9ce3956ad1d7e380260b972f16119521
Ilya Cherkasov <ilya.r.cherkasov@gmail.com>
2023-08-19 23:15:08 +0300

    Another bug

--- Stroyboards/ViewController.swift
+++ Stroyboards/ViewController.swift
@@ -20,6 +20,8 @@ class ViewController: UIViewController {
          Делаем вывод, что connection - это тоже элемент со своим уникальным ID
          Также есть возможность сменить ID элемента
          https://stackoverflow.com/questions/38716293/main-storyboardc-could-not-be-opened-interface-builder-cannot-open-compiled-nib
+         Воспроизводим ошибку
+         Thread 1: "Could not load NIB in bundle: 'NSBundle </Users/ilya/Library/Developer/CoreSimulator/Devices/008593C9-D813-48AF-834B-30468243C9DE/data/Containers/Bundle/Application/E9FE48D3-7C7E-46B8-BF3A-FA9DB22AA382/Stroyboards.app> (loaded)' with name 'ViewControllerID' and directory 'Main.storyboardc'", переименовывая в ипашке файл
          */
         super.init(coder: coder)
     }
