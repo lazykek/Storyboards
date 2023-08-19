d1bcb4cb5a4cd2d1309a25c04492b0c26cc31733
Ilya Cherkasov <ilya.r.cherkasov@gmail.com>
2023-08-19 20:43:07 +0300

    Some explanation

--- Stroyboards/ViewController.swift
+++ Stroyboards/ViewController.swift
@@ -9,12 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private var anotherTestLabel: UILabel!
     @IBOutlet private var testLabel: UILabel!
     @objc private var text: String!
 
     required init?(coder: NSCoder) {
         /*
          storyboard.instantiateViewController -> парсинг -> init(coder: NSCoder)
+         имя переменной (testLabel) связывается с ObjectID
+         зачем нужен  id="oSe-0O-rdk" - ?
          */
         super.init(coder: coder)
     }
