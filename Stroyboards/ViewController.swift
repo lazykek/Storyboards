71233bf3b51ff882132c937f52810042759c94a0
Ilya Cherkasov <ilya.r.cherkasov@gmail.com>
2023-08-19 20:20:06 +0300

    The place where the error occurs

--- Stroyboards/ViewController.swift
+++ Stroyboards/ViewController.swift
@@ -12,12 +12,17 @@ class ViewController: UIViewController {
     @IBOutlet private var testLabel: UILabel!
     @objc private var text: String!
 
+    required init?(coder: NSCoder) {
+        /*
+         storyboard.instantiateViewController -> парсинг -> init(coder: NSCoder)
+         */
+        super.init(coder: coder)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.testLabel.text = "Test"
         print(self.text)
     }
-
-
 }
 
