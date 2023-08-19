2e45b1397324aa09682f1d0b71a60ac3dca4e5e2
Ilya Cherkasov <ilya.r.cherkasov@gmail.com>
2023-08-19 19:37:54 +0300

    Add test label outlet

--- Stroyboards/ViewController.swift
+++ Stroyboards/ViewController.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private var testLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.testLabel.text = "Test"
     }
 
 
