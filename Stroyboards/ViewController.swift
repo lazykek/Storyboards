0194a647c8f5c5b8e228460c49bf0c78fd7a570f
Ilya Cherkasov <ilya.r.cherkasov@gmail.com>
2023-08-20 12:08:52 +0300

    Add swizzling

--- Stroyboards/ViewController.swift
+++ Stroyboards/ViewController.swift
@@ -23,11 +23,7 @@ class ViewController: UIViewController {
          Воспроизводим ошибку
          Thread 1: "Could not load NIB in bundle: 'NSBundle </Users/ilya/Library/Developer/CoreSimulator/Devices/008593C9-D813-48AF-834B-30468243C9DE/data/Containers/Bundle/Application/E9FE48D3-7C7E-46B8-BF3A-FA9DB22AA382/Stroyboards.app> (loaded)' with name 'ViewControllerID' and directory 'Main.storyboardc'", переименовывая в ипашке файл
          */
-        let array = coder.value(forKeyPath: "arrayClass")
-        let table = coder.value(forKeyPath: "keyIDTable") as! NSObject
-        let dictionary = coder.value(forKeyPath: "dictionaryClass") as! NSObject
-        let context = coder.value(forKeyPath: "_storyboardDecodingContext") as! NSObject
-        print(coder.reflected())
+        coder.swizzle()
         super.init(coder: coder)
     }
 
@@ -36,73 +32,37 @@ class ViewController: UIViewController {
         self.testLabel.text = "Test"
         print(self.text)
     }
+
+    override func setValue(_ value: Any?, forKey key: String) {
+        super.setValue(value, forKey: key)
+    }
 }
 
 class CustomLabel: UILabel {
     required init?(coder: NSCoder) {
-        let table = coder.value(forKeyPath: "keyIDTable") as! NSObject
-        print(coder.reflected())
         super.init(coder: coder)
     }
 }
 
-protocol Reflectable: AnyObject {
-    func reflected() -> [String: Any?]
-}
-
-extension Reflectable {
-
-    func reflected() -> [String: Any?] {
-        let mirror = Mirror(reflecting: self)
-        var dict: [String: Any?] = [:]
-        for child in mirror.children {
-            guard let key = child.label else {
-                continue
-            }
-            dict[key] = child.value
+extension NSCoder {
+    func swizzle() {
+        let aClass: AnyClass! = object_getClass(self)
+        let originalMethod = class_getInstanceMethod(aClass, #selector(decodeObject(forKey:)))
+        let swizzledMethod = class_getInstanceMethod(aClass, #selector(decodeObjectSwizzled(forKey:)))
+        if let originalMethod = originalMethod, let swizzledMethod = swizzledMethod {
+            method_exchangeImplementations(originalMethod, swizzledMethod)
         }
-        return dict
     }
 
-    var reflectedString: String {
-        let reflection = reflected()
-        var result = String(describing: self)
-        result += " { \n"
-        for (key, val) in reflection {
-            result += "\t\(key): \(val ?? "null")\n"
+    @objc func decodeObjectSwizzled(forKey key: String) -> Any? {
+        let object = self.decodeObjectSwizzled(forKey: key)
+        DispatchQueue.main.async {
+            print(
+                "KEY: " + key + "\n"
+                + "VALUE: " + object.debugDescription + "\n"
+                + "-----"
+            )
         }
-        return result + "}"
+        return object
     }
-
 }
-
-extension Reflectable where Self: NSObject {
-
-    func reflected() -> [String : Any?] {
-
-        var count: UInt32 = 0
-
-        guard let properties = class_copyPropertyList(Self.self, &count) else {
-            return [:]
-        }
-
-        var dict: [String: Any] = [:]
-        for i in 0..<Int(count) {
-            let name = property_getName(properties[i])
-            guard let nsKey = NSString(utf8String: name) else {
-                continue
-            }
-            let key = nsKey as String
-            guard responds(to: Selector(key)) else {
-                continue
-            }
-            dict[key] = value(forKey: key)
-        }
-        free(properties)
-
-        return dict
-    }
-
-}
-
-extension NSObject: Reflectable {}
